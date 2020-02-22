bef5fb78996fe1c4a8e5b109f704e9417a054f2f
yongzhuo <2714618994@qq.com>
2020-02-22 19:09:19 +0800

    fix example, eg "一万五", "十8.四五6"

--- tookit_sihui/ml_common/chinese_and_number/chinese_and_number.py
+++ tookit_sihui/ml_common/chinese_and_number/chinese_and_number.py
@@ -28,10 +28,13 @@ def is_total_num(text):
 
 
 # chinese_to_number, 单位-数字
-unit_dict = {"十": 10, "百": 100, "千": 1000, "万": 10000, "亿": 100000000}
+unit_dict = {"十": 10, "百": 100, "千": 1000, "万": 10000, "亿": 100000000,
+             "拾":10, "佰":100, "陌":100, "仟":1000, "阡":1000, "萬":10000, "億":100000000}
 unit_dict_keys = unit_dict.keys()
 digit_dict = {"零": 0, "一": 1, "二": 2, "两": 2, "俩": 2, "三": 3,
-             "四": 4, "五": 5, "六": 6, "七": 7, "八": 8, "九": 9}
+             "四": 4, "五": 5, "六": 6, "七": 7, "八": 8, "九": 9,
+             "壹":1, "贰":2, "叁":3, "肆":4, "伍":5, "陆":6, "柒":7, "捌":8, "玖":9, "弌":1, "弍":2, "弎":3,
+             "貳":2, "陸":6, "0":0, "1":1, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9}
 
 # number_to_chinese, 单位-数字
 num_dict = { 0: "零", 1: "一", 2: "二", 3: "三", 4: "四",
@@ -59,8 +62,8 @@ class chinese_to_number():
         :return: 
         """
         assert type(text) == str
-        if "零" in text:
-            text = text.replace("零", "")
+        # if "零" in text:
+        #     text = text.replace("零", "")
         # 分切成三部分
         index = 0
         flag_billion = True # 亿
@@ -105,11 +108,17 @@ class chinese_to_number():
         if not text:
             return 0
         len_text = len(text)
+
+        # 规范语,
         for i in range(len_text):
             # 数字
             if text[i] in digit_dict:
                 number_1 = digit_dict[text[i]]
-                if i == len_text - 1:
+                if i == len_text - 1: # 最后一个数字
+                    if len_text > 2:  # 口语化的情形, 如一万五
+                        if text[len_text - 1] in digit_dict and text[len_text - 2] in unit_dict:
+                            number_res += number_1 * number_2 * 0.1
+                            break
                     number_res += number_1
             # 单位
             elif text[i] in unit_dict:
@@ -128,6 +137,7 @@ class chinese_to_number():
                     number_res = float("".join(text_else_str))
                 except:
                     number_res = 0
+                break
         return number_res
 
     def compose_integer(self, text):
@@ -216,6 +226,8 @@ class chinese_to_number():
                     result_pos += text_end[i]
             # 拼接
             self.result_last = float(self.result_start + result_pos) if result_pos.isdigit() else self.result_start
+        else:
+            self.result_last = self.compose_integer(text)
 
         return self.result_last
 
@@ -270,7 +282,29 @@ class number_to_chinese():
         return res
 
 
+def judge_compose_decimal(data_json):
+    """测试小数"""
+    ctn = chinese_to_number()
+    for data in data_json.keys():
+        res_dec = ctn.compose_decimal(data)
+        if res_dec != data_json[data]:
+            print("dec：\ttrain:" + data + "\ttrue:"+str(data_json[data])+ "\tpred:"+str(res_dec))
+
+
+def judge_compose_integer(data_json):
+    """测试整数"""
+    ctn = chinese_to_number()
+    for data in data_json.keys():
+        res_dec = ctn.compose_integer(data)
+        if res_dec != data_json[data]:
+            print("int：\ttrain:" + data + "\ttrue:"+str(data_json[data])+ "\tpred:"+str(res_dec))
+
+
 if __name__=="__main__":
+    ctn = chinese_to_number()
+    ques = "十9.14六"
+    print(ctn.compose_decimal(ques))
+
     ######  1.测试阿拉伯数字转中文  ######################################################
     ntc = number_to_chinese()
     for i in range(1945, 2100):
@@ -278,32 +312,47 @@ if __name__=="__main__":
     print(ntc.decimal_chinese(0.112354))
     print(ntc.decimal_chinese(1024.112354))
 
-
-
     ######  2.测试中文转阿拉伯   ########################################################
+    # 测试是小数（总接口,包含小数）
+    tet_decimal = {"1.3":1.3, "一.12":1.12, "三千零七十八亿三千零十五万零三百一十二":307830150312.0, "二点13亿":213000000.0, "1.56万": 15600.0,
+                   "十8.12":18.12, "一十八点九一":18.91, "零点一":0.1, "零点123":0.123, "十八点一":18.1, "零":0, "一千零九十九点六六":1099.66}
+    judge_compose_decimal(tet_decimal)
+
+    # 测试是整数(只是整数接口)
     ctn = chinese_to_number()
-    tet_base = [ "一", "一十五", "十五", "二十", "二十三", "一百","一百零一",
-                 "一百一十", "一百一十一", "一千", "一千零一","一千零三十一",
-                 "一万零二十一", "一万零三百二十一", "一万一千三百二十一", "三千零十五万",
-                 "三千零一十五万", "三千五百六十八万零一百零一", "五十亿三千零七十五万零六百二十二",
-                 "十三亿三千零十五万零三百一十二", "一千二百五十八亿","三千零十五万零三百一十二",
-                 "一千二百五十八万亿", "三千三百二十一", "三百三十一", "二十一", "三百二十一",
-                 "一千二百五十八亿零三千三百二十一", "两百", "两千两百二十二", "两亿两千万两百万两百二十二",
-                 "三千零七十八亿三千零十五万零三百一十二"]
-
-    tet_decimal = ["二点13亿", "1.56万", "十八.12", "一十八点九一", "零点一", "零点123", "十八点一", "零", "一千零九十九点六六"]
-    # 测试是小数
-    for tet_d in tet_decimal:
-        print(ctn.compose_decimal(tet_d))
-    # 测试是整数
-    for tet_b in tet_base:
-        print(ctn.compose_integer(tet_b))
+    tet_base = {"一":1, "一十五":15, "十五":15, "二十":20, "二十三":23, "一百":100, "一百零一":101,
+                "一百一十":110, "一百一十一":111, "一千":1000, "一千零一":1001, "一千零三十一":1031,
+                "一万零二十一":10021.0, "一万零三百二十一":10321.0, "一万一千三百二十一":11321.0, "三千零十五万":30150000.0,
+                "三千零一十五万":30150000.0, "三千五百六十八万零一百零一":35680101.0, "五十亿三千零七十五万零六百二十二":5030750622.0,
+                "十三亿三千零十五万零三百一十二":1330150312.0, "一千二百五十八亿":125800000000.0, "三千零十五万零三百一十二":30150312.0,
+                "一千二百五十八万亿":1258000000000000.0, "三千三百二十一":3321.0, "三百三十一":331.0, "二十一":21, "三百二十一":321,
+                "一千二百五十八亿零三千三百二十一":125800003321.0, "两百":200, "两千两百二十二":2222, "两亿两千万两百万两百二十二":222000222.0,
+                "三千零七十八亿三千零十五万零三百一十二":307830150312.0}
+    judge_compose_integer(tet_base)
 
 
+    print(ctn.compose_decimal("三千零七十八亿三千零十五万零三百一十二"))
+    print(ctn.compose_integer("三万亿"))
+    print(ctn.compose_integer("十 "))
+    print(ctn.compose_integer("百"))
+    print(ctn.compose_integer("千"))
+    print(ctn.compose_integer("万"))
+    print(ctn.compose_integer("十万"))
+    print(ctn.compose_integer("百万"))
+    print(ctn.compose_integer("千万"))
+    print(ctn.compose_integer("亿万"))
+    print(ctn.compose_integer("一二一二 "))
+    print(ctn.compose_integer("007"))
+    print(ctn.compose_integer("一百"))
 
 
-
-
-
-
-
+    ctn = chinese_to_number()
+    # 测试异常中文数字
+    cn_num = {"二":2, "三千零七十八亿三千零十五万零三百一十二":307830150312.0, '一千零五':1005, '两':2, '十':10, '一千五':1500,
+              '1000':1000, '贰千伍':2500, '123.05亿万':12305000000.0}
+    judge_compose_decimal(cn_num)
+    while True:
+        print("请输入:")
+        ques = input()
+        print(ctn.compose_decimal(ques))
+        print(ctn.compose_integer(ques))
